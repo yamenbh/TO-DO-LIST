7d9972f103725958db429e7a4f33c0f2a05f4d75
yamenbh <aymaneaitelbhiri14@gmail.com>
2023-12-27 21:06:25 +0100

    seconde commit

---
+++ Todo-Back-End/src/main/java/ma/emsi/todo/TodoBackEndApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackEndApplication.class, args);
+	}
+
+}
---
+++ Todo-Back-End/src/test/java/ma/emsi/todo/TodoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
